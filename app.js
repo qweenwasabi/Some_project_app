48ba1867c18ffdfd4da7e13c42d1d3e88635e9f1
Jacklyne Bouier <jacky_bouier@yahoo.com>
2017-07-07 14:56:19 -0700

    all basic routes up and running

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+// var bodyParser = require('body-parser')
+const app = express();
+app.set('view engine', 'jade');
 
-app.use(express.static('public'))
-
-app.listen(3000, function(){
-  console.log("server up and running")
+// app.use(bodyParser.urlencoded({extended: true}))
+app.get('/', function (req, res) {
+  res.render('index.jade')
 })
+
+app.get('/register', function(req, res){
+  res.render('register.jade')
+})
+
+app.get('/login', function(req, res){
+  res.render('login.jade')
+})
+
+app.get('/dashboard',function(req, res){
+  res.render('dashboard.jade')
+})
+
+app.get('/logout',function(req, res){
+  res.redirect('/')
+})
+
+app.listen(3000);
